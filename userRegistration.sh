23de26fe4cae381dab7288c95e09aa2291bc35e8
AkashSuchak <suchak.akash@gmail.com>
2020-10-22 00:15:46 +0530

    Added : Last Name Validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#As a user need to enter a valid First Name
+#As a user need to enter a valid Last Name
 
+shopt -s extglob
 
 echo "Note : 1) Name Start With capital Letter "
-echo "       2) Minimum 3 Characters required"
-
-#User - Input
-read -p "Enter First Name : " firstName
+echo "       2) Minimum 3 Characters required "
 
 #Regular Expression
-regEx="^[A-Z][a-zA-Z]{2,56}$"
+regEx="^[[:upper:]][[:alpha:]]{2,56}$"
 
-#Checking UserInput is Valid or Not
-if [[ $firstName =~ $regEx ]]; then
-        echo "$firstName is Valid Name"
+#User-Input
+read -p "Enter Last Name : " lastName
+
+#Checking LastName is Valid or Not
+if [[ $lastName =~ $regEx ]]; then
+        echo "$lastName is Valid "
 else
         echo "Wrong Input!! Try Again"
 fi
