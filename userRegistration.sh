710306e98e1dd56f7c8ff155cd0c7bc534cbfe04
AkashSuchak <suchak.akash@gmail.com>
2020-10-21 23:20:09 +0530

    Added : First Name Validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,6 +1,21 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#Welcome Message
+#As a user need to enter a valid First Name
 
-echo "Welcome To User Registration Problem"
+
+echo "Note : 1) Name Start With capital Letter "
+echo "       2) Minimum 3 Characters required"
+
+#User - Input
+read -p "Enter First Name : " firstName
+
+#Regular Expression
+regEx="^[A-Z][a-zA-Z]{2,56}$"
+
+#Checking UserInput is Valid or Not
+if [[ $firstName =~ $regEx ]]; then
+        echo "$firstName is Valid Name"
+else
+        echo "Wrong Input!! Try Again"
+fi
