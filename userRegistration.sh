d817018c632c447d58e923f0b32e39f508e282ee
AkashSuchak <suchak.akash@gmail.com>
2020-10-24 00:55:17 +0530

    Added : Password Validation : 1 Numeric Number

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,19 +5,16 @@
 
 shopt -s extglob
 
-#Regular Expression
-#regEx="(?=.*[A-Z])[a-zA-Z0-9.-@]{8,}$"
-
 #Display example for user
 echo "Note : Enter Password properly"
 echo "-----------------------------"
 echo "Rule:1 - Minimum 8 Characters"
 echo "Rule:2 - Should have Atleast 1 Upper Case"
+echo "Rule:3 - Should have Atleast 1 Numeric Number"
 echo "============================="
 
 #User-Input
 read -p "Enter Your Password : " pass
-
 
 #Checking Password is Valid or Not
 count=`echo ${#pass}`
@@ -25,11 +22,11 @@ if [[ $count -lt 8 ]];then
 	echo "Password length should be 8 charactore"
 	exit 1;
 fi
-echo $pass | grep "[A-Z]"
+echo $pass | grep "[A-Z]" | grep "[0-9]"
 
 if [[ $? -ne 0 ]];then
 
-	echo "Password Must contain 1 upparcase."
+	echo "Password Must contain 1 upparcase, 1 Numeric Number"
 	exit 2;
 else
    echo "Password is Set"
