712aeff8def6549f842669398d7704fd368a307e
AkashSuchak <suchak.akash@gmail.com>
2020-10-24 01:26:11 +0530

    Added : Password Validation : 1 Special Character

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,4 @@
-#! /bin/bash 
+#! /bin/bash
 
 #Author : Akash Suchak
 #As a user need to follow pre-defined Password Rules
@@ -11,6 +11,7 @@ echo "-----------------------------"
 echo "Rule:1 - Minimum 8 Characters"
 echo "Rule:2 - Should have Atleast 1 Upper Case"
 echo "Rule:3 - Should have Atleast 1 Numeric Number"
+echo "Rule:4 - Should have 1 special Character"
 echo "============================="
 
 #User-Input
@@ -22,12 +23,12 @@ if [[ $count -lt 8 ]];then
 	echo "Password length should be 8 charactore"
 	exit 1;
 fi
-echo $pass | grep "[A-Z]" | grep "[0-9]"
-
+echo $pass | grep "[A-Z]" | grep "[0-9]" | grep "[@#$%^&*-_.]"
 if [[ $? -ne 0 ]];then
 
-	echo "Password Must contain 1 upparcase, 1 Numeric Number"
+	echo "Password Must contain 1 upparcase, 1 Numeric Number, 1 Special Character"
 	exit 2;
 else
    echo "Password is Set"
 fi
+
