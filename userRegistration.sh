8127072312612daaa2fe28fe8b0ead13c0af53ef
AkashSuchak <suchak.akash@gmail.com>
2020-10-24 00:33:34 +0530

    Added : Password Valiation : Minimum 1 Uppercase

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash 
 
 #Author : Akash Suchak
 #As a user need to follow pre-defined Password Rules
@@ -6,20 +6,31 @@
 shopt -s extglob
 
 #Regular Expression
-regEx="[0-9a-zA-z#*-_.@ ]{8,}$"
+#regEx="(?=.*[A-Z])[a-zA-Z0-9.-@]{8,}$"
 
 #Display example for user
 echo "Note : Enter Password properly"
 echo "-----------------------------"
 echo "Rule:1 - Minimum 8 Characters"
+echo "Rule:2 - Should have Atleast 1 Upper Case"
 echo "============================="
 
 #User-Input
-read -p "Enter Valid Password : " pwd
+read -p "Enter Your Password : " pass
+
 
 #Checking Password is Valid or Not
-if [[ $pwd =~ $regEx ]]; then
-        echo "Password Entered Correctly"
+count=`echo ${#pass}`
+if [[ $count -lt 8 ]];then
+	echo "Password length should be 8 charactore"
+	exit 1;
+fi
+echo $pass | grep "[A-Z]"
+
+if [[ $? -ne 0 ]];then
+
+	echo "Password Must contain 1 upparcase."
+	exit 2;
 else
-        echo "Invalid Passord !! Try Again"
+   echo "Password is Set"
 fi
